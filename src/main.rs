31eff5c37bfda340348dc3356d5a94ec12b73b64
nokaa <nokaa@cock.li>
2016-03-27 14:15:03 -0500

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ extern crate rotor;
 
 use std::fs::File;
 use std::io::{self, Read, Write, stderr};
-use std::path::{Path, PathBuf};
 use std::str;
 
 use rotor::{EventSet, PollOpt, Loop, Config, Void};
@@ -10,8 +9,69 @@ use rotor::mio::{TryRead, TryWrite};
 use rotor::mio::tcp::{TcpListener, TcpStream};
 use rotor::{Machine, Response, EarlyScope, Scope};
 
-mod response;
+fn main() {
+    let root_dir = String::from(".");
+    let context = Context {
+        root_dir: root_dir,
+    };
 
+    let mut loop_creator = Loop::new(&Config::new()).unwrap();
+    let lst = TcpListener::bind(&"127.0.0.1:3000".parse().unwrap()).unwrap();
+    loop_creator.add_machine_with(|scope| {
+        Gopher::new(lst, scope)
+    }).unwrap();
+    loop_creator.run(context).unwrap();
+}
+
+fn local_path_for_request(request_path: &[u8], root_dir: &String) -> Option<Vec<u8>> {
+    // Check that request is a path
+    if request_path[0] != b'/' {
+        return None;
+    }
+
+    // Append the requested path to the root directory
+    let mut path = root_dir.clone().into_bytes();
+    let mut request_path = get_nonzero_bytes(request_path);
+    request_path.pop();
+    path.append(&mut request_path);
+
+    if path[path.len() - 1] == b'/' {
+        path.append(&mut String::from("index.gph").into_bytes());
+    }
+
+    let path = String::from_utf8(path).unwrap();
+
+    // Read file
+    let f = match read_file(path) {
+        Ok(f) => f,
+        Err(_) => {
+            return None
+        }
+    };
+
+    Some(f)
+}
+
+fn read_file(filename: String) -> Result<Vec<u8>, io::Error> {
+    let mut f = try!(File::open(filename));
+    let mut buf: Vec<u8> = vec![];
+    try!(f.read_to_end(&mut buf));
+
+    Ok(buf)
+}
+
+fn get_nonzero_bytes(data: &[u8]) -> Vec<u8> {
+    let mut buf: Vec<u8> = vec![];
+    for &ch in data {
+        if ch == 0u8 {
+            continue;
+        } else {
+            buf.push(ch);
+        }
+    }
+
+    buf
+}
 
 struct Context {
     root_dir: String,
@@ -78,7 +138,7 @@ impl Machine for Gopher {
                     Ok(Some(0)) => {
                         Response::done()
                     }
-                    Ok(Some(x)) => {
+                    Ok(Some(_)) => {
                         let data_str = str::from_utf8(&data).unwrap();
                         println!("{}", data_str);
                         match local_path_for_request(&data, &scope.root_dir) {
@@ -93,18 +153,6 @@ impl Machine for Gopher {
                             }
                             None => Response::done(),
                         }
-                        //Response::done()
-                        /*match sock.try_write(&data[..x]) {
-                            Ok(_) => {
-                                // this is example so we don't care if not all
-                                // (or none at all) bytes are written
-                                Response::ok(Gopher::Connection(sock))
-                            }
-                            Err(e) => {
-                                writeln!(&mut stderr(), "write: {}", e).ok();
-                                Response::done()
-                            }
-                        }*/
                     }
                     Ok(None) => {
                         Response::ok(Gopher::Connection(sock))
@@ -130,68 +178,4 @@ impl Machine for Gopher {
     {
         unreachable!();
     }
-}
-
-fn main() {
-    let root_dir = String::from(".");
-    let context = Context {
-        root_dir: root_dir,
-    };
-
-    let mut loop_creator = Loop::new(&Config::new()).unwrap();
-    let lst = TcpListener::bind(&"127.0.0.1:3000".parse().unwrap()).unwrap();
-    loop_creator.add_machine_with(|scope| {
-        Gopher::new(lst, scope)
-    }).unwrap();
-    loop_creator.run(context).unwrap();
-}
-
-fn local_path_for_request(request_path: &[u8], root_dir: &String) -> Option<Vec<u8>> {
-    // Check that request is a path
-    if request_path[0] != b'/' {
-        return None;
-    }
-
-    // Append the requested path to the root directory
-    let mut path = root_dir.clone().into_bytes();
-    let mut request_path = get_nonzero_bytes(request_path);
-    request_path.pop();
-    path.append(&mut request_path);
-
-    if path[path.len() - 1] == b'/' {
-        path.append(&mut String::from("index.gph").into_bytes());
-    }
-
-    let path = String::from_utf8(path).unwrap();
-
-    // Read file
-    let f = match read_file(path) {
-        Ok(f) => f,
-        Err(_) => {
-            return None
-        }
-    };
-
-    Some(f)
-}
-
-fn read_file(filename: String) -> Result<Vec<u8>, io::Error> {
-    let mut f = try!(File::open(filename));
-    let mut buf: Vec<u8> = vec![];
-    try!(f.read_to_end(&mut buf));
-
-    Ok(buf)
-}
-
-fn get_nonzero_bytes(data: &[u8]) -> Vec<u8> {
-    let mut buf: Vec<u8> = vec![];
-    for &ch in data {
-        if ch == 0u8 {
-            continue;
-        } else {
-            buf.push(ch);
-        }
-    }
-
-    buf
 }
