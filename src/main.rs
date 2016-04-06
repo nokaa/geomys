8d499a8c3f74855e4b4b22c11806996e8f8c5408
nokaa <nokaa@cock.li>
2016-04-06 01:26:36 -0500

    Add TOML parsing

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@
 
 extern crate clap;
 extern crate rotor;
+extern crate rustc_serialize;
+extern crate toml;
 
 mod gopher;
 mod util;
@@ -14,8 +16,13 @@ mod util;
 use clap::App;
 use rotor::{Loop, Config};
 use rotor::mio::tcp::TcpListener;
+use rustc_serialize::Decoder;
 
 use gopher::{Context, Gopher};
+
+use std::env;
+use std::fs::File;
+use std::io::Read;
 
 fn main() {
     let matches = App::new("geomys")
@@ -27,8 +34,40 @@ fn main() {
             [ROOT] 'Sets the root dir (default \".\")'")
         .get_matches();
 
-    let addr = matches.value_of("ADDR").unwrap_or("0.0.0.0:70");
-    let root_dir = String::from(matches.value_of("ROOT").unwrap_or("."));
+    // Get the user's home directory for attempting to read
+    // ~/.config/geomys/config
+    let mut home = env::home_dir().unwrap();
+    home.push(".config/geomys/config");
+    let home = home.to_str().unwrap();
+
+    let addr: String;
+    let root_dir: String;
+
+    if let Some(a) = matches.value_of("ADDR") {
+        addr = String::from(a);
+    } else if let Some(a) = val_exists(home, String::from("address")) {
+        println!("reading address from {}", home);
+        addr = a;
+    } else if let Some(a) = val_exists("/etc/geomys/config", String::from("address")) {
+        println!("reading address from /etc/geomys/config");
+        addr = a;
+    } else {
+        println!("using default value for addr");
+        addr = String::from("0.0.0.0:70");
+    }
+
+    if let Some(dir) = matches.value_of("ROOT") {
+        root_dir = String::from(dir);
+    } else if let Some(dir) = val_exists(home, String::from("root_dir")) {
+        println!("reading root_dir from {}", home);
+        root_dir = dir;
+    } else if let Some(dir) = val_exists("/etc/geomys/config", String::from("root_dir")) {
+        println!("reading root_dir from /etc/geomys/config");
+        root_dir = dir;
+    } else {
+        println!("using default value for root_dir");
+        root_dir = String::from("/var/gopher");
+    }
 
     println!("addr: {}", addr);
     println!("root dir: {}", root_dir);
@@ -44,4 +83,38 @@ fn main() {
         Gopher::new(lst, scope)
     }).unwrap();
     loop_creator.run(context).unwrap();
+}
+
+fn val_exists(filename: &str, value: String) -> Option<String> {
+    if !file_exists(filename) {
+        return None;
+    }
+
+    let mut f = File::open(filename).unwrap();
+    let mut tom = String::new();
+    match f.read_to_string(&mut tom) {
+        Ok(_) => { },
+        Err(_) => return None,
+    }
+
+    let mut parser = toml::Parser::new(&tom);
+    let table = match parser.parse() {
+        Some(t) => t,
+        None => return None,
+    };
+
+    match table.get(&value) {
+        Some(val) => match toml::Decoder::new(val.clone()).read_str() {
+            Ok(s) => return Some(s),
+            Err(_) => return None,
+        },
+        None => return None,
+    }
+}
+
+fn file_exists(filename: &str) -> bool {
+    match File::open(filename) {
+        Ok(_) => true,
+        Err(_) => false,
+    }
 }
